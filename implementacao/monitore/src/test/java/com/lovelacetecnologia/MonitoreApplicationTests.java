25c6ba5704a597435bc0bc17e571323c613fa15f
rafael <rafael@rafael-amaral>
2020-09-14 18:00:38 -0300

    Criação Proejeto

---
+++ implementacao/monitore/src/test/java/com/lovelacetecnologia/MonitoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovelacetecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
